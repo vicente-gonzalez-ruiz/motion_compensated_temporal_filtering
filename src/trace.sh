50ab76e6176d5a821350d32f57339ed6f01e08d1
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2017-12-09 21:45:58 +0100

    .

--- src/trace.sh
+++ src/trace.sh
@@ -1,13 +1,5 @@
 #! /bin/bash
 
-
-## \file trace.sh
-#
-#  \brief Register to a log file.
-#
-#  \author Vicente Gonzalez-Ruiz.
-#  \date Last modification: 2015, January 7.
-
 echo $@ >> trace
 $@
 exit $?
