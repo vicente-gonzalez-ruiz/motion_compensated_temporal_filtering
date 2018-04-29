861f08fe6f31f9fa7a77c00dd40a376bc1118959
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2018-04-29 19:45:26 +0200

    .

--- src/transcode_quality.py
+++ src/transcode_quality.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#!/home/vruiz/.pyenv/shims/python -i
 # -*- coding: iso-8859-15 -*-
 
 # Quality transcoding. Extracts a codestream from a longest one,
@@ -106,11 +107,13 @@ from   subprocess       import CalledProcessError
 from   arguments_parser import arguments_parser
 import io
 import operator
+import math
 
 # {{{ Logging
 
 logging.basicConfig()
 log = logging.getLogger("transcode_quality")
+log.setLevel('INFO')
 
 # }}}
 
@@ -133,6 +136,10 @@ layers = int(args.layers)
 #quality = float(args.quality)
 #quantization_step = int(args.quantization_step)
 TRLs = int(args.TRLs)
+
+log.info("GOPs={}".format(GOPs))
+log.info("layers={}".format(layers))
+log.info("TRLs={}".format(TRLs))
 
 # }}} Arguments parsing
 
@@ -169,57 +176,66 @@ subband_layers = []
     
 # L
 for q in range(quality_layers):
-    subband_layers.append(('L', TRLs, q, 1/math.sqrt(2.0)^q))
+    subband_layers.append(('L', TRLs, q, 1.0/(math.sqrt(2.0)**q)))
 
 # H's
 for s in range(TRLs,1):
     for q in range(quality_layers):
-        subband_layers.append(('H', s, q, 1/gains[s]/math.sqrt(2.0)^q))
+        subband_layers.append(('H', s, q, 1.0/(gains[s]/(math.sqrt(2.0)**q))))
 
+log.info("subband_layers={}".format(subband_layers))
+        
 # }}} subband_layers: a list of tuples ('L'|'H', subband, layer, relative slope)
 
 # Sort the subband-layers by their relative slope
-sorted = subband_layers.sort(key=operator.itemgetter(3))
+subband_layers.sort(key=operator.itemgetter(3))
 
 # Truncate the list
-truncated = del sorted[layers:]
+del subband_layers[layers:]
 
 # {{{ Count the number of subband-layers per subband
 layers = {}
 layers[('L', TRLs)] = 0
 for i in range(TRLs):
     layers[('H', i)] = 0
-for i in truncated:
-    layers[(i[0], i[1]] += 1
+for i in subband_layers:
+    layers[(i[0], i[1])] += 1
 # }}}
 
+log.info("layers={}".format(layers))
+
 def transcode_image(filename, layers):
-    try:
-        check_call("trace kdu_transcode"
-                   + " -i " + filename
-                   + " -o " + "transcode_quality/" + filename,
-                   + " Clayers=" + str(layers), 
-                   shell=True)
-    except CalledProcessError:
-        sys.exit(-1)
+    print(filename, layers)
+    if layers > 0:
+        try:
+            check_call("trace kdu_transcode"
+                       + " -i " + filename
+                       + " -o " + "transcode_quality/" + filename
+                       + " Clayers=" + str(layers), 
+                       shell=True)
+        except CalledProcessError:
+            sys.exit(-1)
 
 # {{{ GOPs and pictures
 
 gop=GOP()
 GOP_size = gop.get_size(TRLs)
-log.debug("GOP_size = {}".format(GOP_size))
+log.info("GOP_size = {}".format(GOP_size))
 
 pictures = (GOPs - 1) * GOP_size + 1
-log.debug("pictures = {}".format(pictures))
+log.info("pictures = {}".format(pictures))
 
 # }}}
+
+LOW = "low"
+HIGH = "high"
 
 # Transcoding of H subbands
 subband = 1
 while subband < TRLs:
 
-    pictures = (pictures + 1) // 2 - 1
-    log.debug("Transcoding subband H[{}] of {} pictures".format(subband, pictures))
+    pictures = (pictures + 1) // 2
+    log.info("Transcoding subband H[{}] with {} pictures".format(subband, pictures))
     
     image_number = 0
     while image_number < pictures:
@@ -227,239 +243,32 @@ while subband < TRLs:
         str_image_number = '%04d' % image_number
 
         filename = HIGH + "_" + str(subband) + "_" + str_image_number + "_Y" 
-        transcode_image(filename + ".j2c", slope[subband])
+        transcode_image(filename + ".j2c", layers[('H', subband)])
 
         filename = HIGH + "_" + str(subband) + "_" + str_image_number + "_U" 
-        transcode_image(filename + ".j2c", slope[subband])
+        transcode_image(filename + ".j2c", layers[('H', subband)])
 
         filename = HIGH + "_" + str(subband) + "_" + str_image_number + "_V" 
-        transcode_image(filename + ".j2c", slope[subband])
+        transcode_image(filename + ".j2c", layers[('H', subband)])
 
         image_number += 1
 
     subband += 1
 
 # Transcoding of L subband
-pictures - 1:
+log.info("Transcoding subband L[{}] with {} pictures".format(subband, pictures))
+image_number = 0
+while image_number < pictures:
 
     str_image_number = '%04d' % image_number
 
     filename = LOW + "_" + str(TRLs-1) + "_" + str_image_number + "_Y"
-    kdu_transcode(filename + ".j2c", number_of_quality_layers_in_L)
+    transcode_image(filename + ".j2c", layers[('L', TRLs)])
 
     filename = LOW + "_" + str(TRLs-1) + "_" + str_image_number + "_U"
-    kdu_transcode(filename + ".j2c", number_of_quality_layers_in_L)
+    transcode_image(filename + ".j2c", layers[('L', TRLs)])
 
     filename = LOW + "_" + str(TRLs-1) + "_" + str_image_number + "_V"
-    kdu_transcode(filename + ".j2c", number_of_quality_layers_in_L)
+    transcode_image(filename + ".j2c", layers[('L', TRLs)])
 
     image_number += 1
-
-
-sys.quit()
-# -------
-
-
-        
-# {{{ Slope computation of each subband-layer of each temporal subband
-
-if   TRLs == 1 :
-    pass
-elif TRLs == 2 :
-    gain = [1.0, 1.2460784922] # [L1/H1]
-elif TRLs == 3 :
-    gain = [1.0, 1.2500103877, 1.8652117304] # [L2/H2, L2/H1]
-elif TRLs == 4 :
-    gain = [1.0, 1.1598810146, 2.1224082769, 3.1669663339]
-elif TRLs == 5 :
-    gain = [1.0, 1.0877939347, 2.1250255455, 3.8884779989, 5.8022196044]
-elif TRLs == 6 :
-    gain = [1.0, 1.0456562538, 2.0788785438, 4.0611276369, 7.4312544148, 11.0885981772]
-elif TRLs == 7 :
-    gain = [1.0, 1.0232370223, 2.0434169985, 4.0625355976, 7.9362383342, 14.5221257323, 21.6692913386]
-elif TRLs == 8 :
-    gain = [1.0, 1.0117165706, 2.0226778348, 4.0393126714, 8.0305936232, 15.6879129862, 28.7065276104, 42.8346456693]
-else :
-    sys.stderr.write("Gains are not available for " + str(TRLs) + " TRLs. Enter them in transcode_quality.py")
-    exit (0)
-
-# {{{ Typical range of useful slopes in Kakadu
-MAX_SLOPE = 50000 # Min quality
-MIN_SLOPE = 40000 # Max quality
-RANGE_SLOPES = MAX_SLOPE - MIN_SLOPE
-# }}}
-
-Q_STEP = 256 # In Kakadu, this should avoid the generation of empty layers
-
-#slope = [[0 for l in range(layers)] for t in range(TRLs)]
-slope = []
-slope.append([])
-#layer = 0
-with io.open('slopes.txt', 'r') as file:
-    for line in file:
-        slope[0].append(int(line))
-        #slope[0][layer] = int(line)
-        #layer += 1
-
-# TRLs = 1
-#  L^0
-# TRLs = 2
-#  L^1, H^1
-# TRLs = 3
-#  L^2, H^2, H^1
-# :
-
-for s in range(1,TRLs):
-    slope.append([])
-    for l in range(layers):
-        slope[s].append(int(slope[0][l]*gain[s]))
-
-for l in range(layers):
-    for s in range(TRLs):
-        print('{} '.format(slope[s][l]), end="")
-    print()
-
-# {{{ Compute slopes
-#for s in range(TRLs):
-#    log.debug("Temporal subband {}".format(s))
-#    for q in range(layers):
-#        log.debug("Subband-layer {}".format(q))
-#        _slope_ = int(round(MAX_SLOPE - quality - Q_STEP*q) / GAINS[s])
-#        if _slope_ < 0:
-#            slope[s][q] = (0, s, q)
-#        else:
-#            slope[s][q] = (_slope_, s, q)
-#        log.debug("Slope {}".format(slope[s][q]))
-# }}}
-
-# }}}
-
-#print("{}".format(slope))
-        
-# {{{ GOPs and pictures
-
-gop=GOP()
-GOP_size = gop.get_size(TRLs)
-log.debug("GOP_size = {}".format(GOP_size))
-
-pictures = (GOPs - 1) * GOP_size + 1
-log.debug("pictures = {}".format(pictures))
-
-# }}}
-
-# {{{ Transcode the (texture) subbands
-
-LOW = "low"
-HIGH = "high"
-MOTION = "motion_residue"
-
-def kdu_transcode(filename, layers):
-    try:
-        check_call("trace kdu_transcode"
-                   + " -i " + filename
-                   + " -o " + "transcode_quality/" + filename,
-                   + " Clayers=" + str(layers), 
-                   shell=True)
-    except CalledProcessError:
-        sys.exit(-1)
-
-# Transcoding of H subbands
-subband = 1
-while subband < TRLs:
-
-    pictures = (pictures + 1) // 2 - 1
-    log.debug("Transcoding subband H[{}] of {} pictures".format(subband, pictures))
-    
-    image_number = 0
-    while image_number < pictures:
-
-        str_image_number = '%04d' % image_number
-
-        filename = HIGH + "_" + str(subband) + "_" + str_image_number + "_Y" 
-        kdu_transcode(filename + ".j2c", slope[subband])
-
-        filename = HIGH + "_" + str(subband) + "_" + str_image_number + "_U" 
-        kdu_transcode(filename + ".j2c", slope[subband])
-
-        filename = HIGH + "_" + str(subband) + "_" + str_image_number + "_V" 
-        kdu_transcode(filename + ".j2c", slope[subband])
-
-        image_number += 1
-
-    subband += 1
-
-
-# We know the number of subband-layers (quality layers of each
-# temporal subband): texture_layers and the number of subbands
-# (TRLs). If for example, texture_layers (Q)==8, and TRLs (T)==5, the
-# order of subband-layers should be: L^4_7 (one quality layer of L^4),
-# L^4_6 (two quality levels of L^4), M^4 (the first and only layer of
-# M4), L^4_5 (in total, tree quality layers of L^4), 
-
-# Q=4, T=5:
-
-# q=1: L^4_3
-# q=2: L^4_2, M^4, H^4_3
-# q=3: L^4_1,      H^4_2, M^3, H^3_3
-# q=4: L^4_0,      H^4_1,      H^3_2, M^2, H^2_3
-# q=5:             H^4_0,      H^3_1,      H^2_2
-# q=6:                         H^3_0,      H^2_1, M^1, H^1_3
-# q=7:                                     H^2_0,      H^1_2
-# q=8:                                                 H^1_1
-# q=9:                                                 H^1_0
-
-#### OLD ####
-
-# Transcoding of M "subbands"
-'''
-subband = 1
-pictures = GOPs * GOP_size - 1
-fields = pictures // 2
-while subband < TRLs:
-
-    field = 0
-    
-    while field < fields:
-
-        str_field = '%04d' % field
-
-        for component in range(4):
-            
-            filename = MOTION + "_" + str(subband) + "_" + str_field + "_comp" + str(component) + ".j2c"
-            kdu_transcode(filename, number_of_quality_layers_in_M[subband])
-
-        field += 1
-
-    fields /= 2
-
-    subband += 1
-'''
-
-# Transcoding of L subband image_number = 0 while image_number <
-pictures - 1:
-
-    str_image_number = '%04d' % image_number
-
-    filename = LOW + "_" + str(TRLs-1) + "_" + str_image_number + "_Y"
-    kdu_transcode(filename + ".j2c", number_of_quality_layers_in_L)
-
-    filename = LOW + "_" + str(TRLs-1) + "_" + str_image_number + "_U"
-    kdu_transcode(filename + ".j2c", number_of_quality_layers_in_L)
-
-    filename = LOW + "_" + str(TRLs-1) + "_" + str_image_number + "_V"
-    kdu_transcode(filename + ".j2c", number_of_quality_layers_in_L)
-
-    image_number += 1
-
-'''
-subband = 1
-while subband < TRLs:
-
-    try:
-        check_call("cp frame_types_" + str(subband) + " transcode_quality/",
-                   shell=True)
-    except CalledProcessError:
-        sys.exit(-1)
-
-    subband += 1
-'''
