dffc3a677dcee3876dac2cefe0a93a32ef5f1dab
Vicente Gonzalez Ruiz <vruiz@io.ual.es>
2018-07-15 08:28:34 +0200

    .

---
+++ experiments/container_352x288x30x420x300.sh
@@ -0,0 +1,8 @@
+nohup ~/MCTF-video-coding/experiments/DRcurve.sh -v /home/vruiz/Videos/container_352x288x30x420x300.avi -g 129 -t 1 &
+nohup ~/MCTF-video-coding/experiments/DRcurve.sh -v /home/vruiz/Videos/container_352x288x30x420x300.avi -g 65 -t 2 &
+nohup ~/MCTF-video-coding/experiments/DRcurve.sh -v /home/vruiz/Videos/container_352x288x30x420x300.avi -g 33 -t 3 &
+nohup ~/MCTF-video-coding/experiments/DRcurve.sh -v /home/vruiz/Videos/container_352x288x30x420x300.avi -g 17 -t 4 &
+nohup ~/MCTF-video-coding/experiments/DRcurve.sh -v /home/vruiz/Videos/container_352x288x30x420x300.avi -g 9 -t 5 &
+nohup ~/MCTF-video-coding/experiments/DRcurve.sh -v /home/vruiz/Videos/container_352x288x30x420x300.avi -g 5 -t 6 &
+nohup ~/MCTF-video-coding/experiments/DRcurve.sh -v /home/vruiz/Videos/container_352x288x30x420x300.avi -g 3 -t 7 &
+nohup ~/MCTF-video-coding/experiments/DRcurve.sh -v /home/vruiz/Videos/container_352x288x30x420x300.avi -g 2 -t 8 &
